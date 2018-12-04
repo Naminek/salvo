941d241854fb0f5d0018c6c630b2dcc97ca3c476
Naminek <nam.k.0519@gmail.com>
2018-12-04 15:33:32 +0100

    create game with date

--- src/main/java/com/codeoftheweb/salvo/Game.java
+++ src/main/java/com/codeoftheweb/salvo/Game.java
@@ -1,4 +1,44 @@
 package com.codeoftheweb.salvo;
 
+
+import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+@Entity
 public class Game {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private long id;
+    private String currentDate;
+
+    public Game() {
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String toString() {
+        return currentDate;
+    }
 }
+
