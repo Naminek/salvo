fe46f136f8ab055bd21dc095d191b899fc713a21
Naminek <nam.k.0519@gmail.com>
2018-12-10 17:19:30 +0100

    got only one player in game_view

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -1,8 +1,10 @@
 package com.codeoftheweb.salvo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -15,9 +17,14 @@ import java.util.List;
 @RequestMapping("/api")
 public class SalvoController {
 
-    @Autowired
     private GameRepository gameRepo;
     private GamePlayerRepository gamePlayerRepo;
+
+    @Autowired
+    public SalvoController(GameRepository gameRepo, GamePlayerRepository gamePlayerRepo) {
+        this.gameRepo = gameRepo;
+        this.gamePlayerRepo = gamePlayerRepo;
+    }
 
 
     @RequestMapping("/games")
@@ -27,27 +34,24 @@ public class SalvoController {
             put("created", game.getCreatedDate());
             put("gamePlayers", game.getGamePlayers().stream().map(gamePlayer -> new HashMap<String, Object>() {{
                 put("id", gamePlayer.getGamePlayerId());
-                put("player", new HashMap<String, Object>() {{
-                    put("id", gamePlayer.getPlayer().getPlayerId());
-                    put("email", gamePlayer.getPlayer().getEmail());
-                }});
+                put("player", createPlayerMap(gamePlayer.getPlayer()));
 
             }}).collect(Collectors.toList()));
         }}).collect(Collectors.toList());
     }
 
-    @RequestMapping("/game_view/{nn}")
-    public List<Map<String, Object>> getGameView(@PathVariable("nn") Long gamePlayerId) {
-        return gameRepo.findAll().stream().map(game -> new HashMap<String, Object>() {{
-            put("id", game.getGameId());
-            put("created", game.getCreatedDate());
-            put("gamePlayers", game.getGamePlayers().stream().map(gamePlayer -> new HashMap<String, Object>() {{
-                put("id", gamePlayer.getGamePlayerId());
-                put("player", new HashMap<String, Object>() {{
-                    put("id", gamePlayer.getPlayer().getPlayerId());
-                    put("email", gamePlayer.getPlayer().getEmail());
-                }});
-            }}).collect(Collectors.toList()));
-        }}).collect(Collectors.toList());
+    private Map<String, Object> createPlayerMap(Player player) {
+        return new HashMap<String, Object>() {{
+            put("id", player.getPlayerId());
+            put("email", player.getEmail());
+        }};
+    }
+
+    @RequestMapping(value = "/game_view/{nn}", method = RequestMethod.GET)
+    public Map<String, Object> getGameView(@PathVariable("nn") Long gamePlayerId) {
+
+        return new HashMap<String, Object>() {{
+            put("id", gamePlayerRepo.findOne(gamePlayerId).getGame().getGameId());
+        }};
     }
 }
