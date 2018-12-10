524e03086888482a6310ba99ad857e4f6a833732
Naminek <nam.k.0519@gmail.com>
2018-12-10 17:43:36 +0100

    finish monday

--- src/main/java/com/codeoftheweb/salvo/SalvoController.java
+++ src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -1,7 +1,6 @@
 package com.codeoftheweb.salvo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -52,6 +51,8 @@ public class SalvoController {
 
         return new HashMap<String, Object>() {{
             put("id", gamePlayerRepo.findOne(gamePlayerId).getGame().getGameId());
+            put("created", gamePlayerRepo.findOne(gamePlayerId).getGame().getCreatedDate());
+            put("gamePlayers", createPlayerMap(gamePlayerRepo.findOne(gamePlayerId).getPlayer()));
         }};
     }
 }
